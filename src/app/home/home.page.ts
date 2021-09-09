4e51ac6653abe67f87d7f61c25114c8ba74fe542
Your Name <jbecofla@gmail.com>
2021-09-08 21:44:13 -0300

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  
+  dato: string;
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+  ngOnInit() {}
+  saludar() {
+    this.presentToast("Bienvenido " + this.dato + "!");
+  }
+
+  async presentToast(msg:string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
